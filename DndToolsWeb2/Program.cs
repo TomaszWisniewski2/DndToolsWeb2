9b9a8f4b142125145011ea0a63fc063acc414ce3
xmen2237 <45273004+TomaszWisniewski2@users.noreply.github.com>
2025-08-16 00:52:11 +0200

    coś2

--- DndToolsWeb2/Program.cs
+++ DndToolsWeb2/Program.cs
@@ -1,27 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSingleton<ScraperService>();
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseDefaultFiles(); // aby index.html dzia³a³ bez œcie¿ki
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseStaticFiles(); // Umo¿liwia serwowanie plików statycznych
 
 app.UseRouting();
+
 app.MapControllers();
 
-// dodaj fallback dla SPA / index.html
+// Obs³uguje nieznane œcie¿ki i przekierowuje do index.html
 app.MapFallbackToFile("index.html");
 
 app.Run();
